6b9b29366c3f06945149c6a4c0027d8fcb98a3c2
Nick West <sirnickthebold@gmail.com>
2024-11-04 08:41:28 -0500

    add pygame template

---
+++ pygame_template/game.py
@@ -0,0 +1,34 @@
+class Game():
+  def __init__(self, title, width, height, bg_color) -> None:
+    self.title = title
+    self.width = width
+    self.height = height
+    self.bg_color = bg_color
+    self.screen, self.background = self.create_window()
+    self.clock = pygame.time.Clock()
+    self.screen.blit(self.background, (0,0))
+    pygame.init()
+
+  def create_window(self):
+    """ return screen and background objects """
+    screen = pygame.display.set_mode(
+      (self.width,self.height),
+      HWSURFACE|DOUBLEBUF)
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill(self.bg_color)
+    screen.blit(background, (0,0))
+    pygame.display.flip()
+    screen.fill(self.bg_color)
+    pygame.display.set_caption(self.title)
+    return screen, background
+
+  def handle_input(self):
+    """ handle events and keypresses """
+    for event in pygame.event.get():
+      if event.type == QUIT:
+        return
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_END] or keys[pygame.K_ESCAPE]:
+      sys.exit(0)
